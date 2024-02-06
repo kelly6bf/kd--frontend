4d037f9370e4e45e2d3c4f31766e6caa1ca90a9f
edgar6bf <edgar6bf@gmail.com>
2024-02-07 02:35:24 +0900

    feat: Navbar 초안 구현

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import {VscNotebook} from "react-icons/vsc";
+import {BsPencil} from "react-icons/bs";
+
+export default function Navbar() {
+  return (
+    <header className="flex justify-between items-center px-4 py-4 border-b border-gray-300">
+      <Link to="/" className="flex items-center gap-2 m-auto text-4xl font-bold text-brand">
+        <VscNotebook />
+        <h1>Kelly's Day</h1>
+      </Link>
+      <nav className="flex items-center space-x-4 p-2 text-xl font-semibold">
+        <Link to={"/diaries/new"}>
+          <BsPencil/>
+        </Link>
+        <Link to="/diaries/me">
+          My Page
+        </Link>
+        <Link to="/login">
+          Login
+        </Link>
+      </nav>
+    </header>
+  );
+}
